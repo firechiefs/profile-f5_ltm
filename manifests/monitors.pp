e515de0d6c6761d5a269aca889b2e6abcb469ba5
Rodrigo Marin III <rodrigo.mariniii@veritas.com>
2016-04-18 14:10:12 -0700

    Fleshed out the monitors manifest

--- manifests/monitors.pp
+++ manifests/monitors.pp
@@ -1,5 +1,13 @@
 # == Class: profile_f5_ltm::monitors
-#
+# creates monitors for all specified roles_to_lb
 class profile_f5_ltm::monitors {
-  # resources
+  # similar pattern as nodes:
+  # get all 'monitor' keys of a role to lb, and pass that hash into a
+  # create_resources() call with the f5_monitor type
+
+  $profile_f5_ltm::roles_to_lb.keys.each | $role | {
+    debug("Creating Resource f5_monitor with data: ${monitor}")
+    $monitor = $profile_f5_ltm::roles_to_lb[$role][monitor]
+    create_resources(f5_monitor,$monitor)
+
 }
