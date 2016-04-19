fb06bf070003c043096fab91220c6daa53e5a622
Rodrigo Marin III <rodrigo.mariniii@veritas.com>
2016-04-19 13:45:39 -0700

    back to symbols, problem appears to be a hiera structure issue

--- manifests/pools.pp
+++ manifests/pools.pp
@@ -19,13 +19,13 @@ class profile_f5_ltm::pools {
   # ]
 
   $profile_f5_ltm::roles_to_lb.keys.each | $role | {
-    $pool_hash = $profile_f5_ltm::roles_to_lb[$role]['pool']
+    $pool_hash = $profile_f5_ltm::roles_to_lb[$role][pool]
     Notify {'pool hash':
       message => $pool_hash,
     }
 
-    $port = $pool_hash[$role]['listening_port']
-    $partition = $profile_f5_ltm::roles_to_lb[$role]['partition']
+    $port = $pool_hash[$role][listening_port]
+    $partition = $profile_f5_ltm::roles_to_lb[$role][partition]
 
     # usage: generate_members_hash_array("puhprxs",80,"/INF")
     $getmembers = generate_members_hash_array($role,$port,$partition)
