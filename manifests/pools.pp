97b27c172e23252cfc356ec0f1e9ad67377e1671
Rodrigo Marin III <rodrigo.mariniii@veritas.com>
2016-04-19 10:22:14 -0700

    removed bad var

--- manifests/pools.pp
+++ manifests/pools.pp
@@ -19,8 +19,6 @@ class profile_f5_ltm::pools {
   # ]
 
 
-  $nodenames = $profile_f5_ltm::nodes::nodes.keys
-
   $test = generate_members_hash_array("puhprx",80,"/INF")
   Notify {'another test':
       message => "this is a test inside f5_ltm_pools"
