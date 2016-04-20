1c9f6a835f42262b7a74cf71ac22810a86607d9f
Rodrigo Marin III <rodrigo.mariniii@veritas.com>
2016-04-19 17:21:37 -0700

    added implementation for virtual servers

--- manifests/vip.pp
+++ manifests/vip.pp
@@ -1,5 +1,20 @@
 # == Class: profile_f5_ltm::vip
-#
+# creates virtual servers for all roles_to_lb
 class profile_f5_ltm::vip {
-  # resources
+  # This wraps the f5_virtualserver type, which is a very complicated type:
+  # https://forge.puppet.com/puppetlabs/f5/readme#f5_virtualserver
+  # There are combinations of features that are only available with certain
+  # 'providers'
+  # See the providers and features blurb in the above link for valid combntns
+  # our use case, probably, is within the 'standard' provider.
+  # All the magic happens in the f5_virtualserver type
+  # We are only passing the options via hiera via create_resources
+
+  # use the keys of roles_to_lb, which are role names
+  $profile_f5_ltm::roles_to_lb.keys.each | $role | {
+    $virtual_server = $profile_f5_ltm::roles_to_lb["${role}"][vip]
+    validate_hash($virtual_server)
+    create_resources(f5_monitor,$virtual_server)
+  }
+
 }
